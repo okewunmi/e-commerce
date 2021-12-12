cf0dcf4ab616648fe870c3ee993b8275fe3c9c66
okewunmi Afeez <okewunmiafeezolaide@mail.com>
2021-12-12 15:17:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// document.getElementById("login-form").addEventListener('submit',(event)=>{
+//  event.preventDefault()
+// })
+// firebase.auth().onAuthStateChanged((user)=>{
+//  if (user)(
+//   location.replace("form.html")
+//  )
+// });
+
+// firebase.auth().onAuthStateChanged((user)=>{
+//  if (!user)(
+//   location.replace("login.html")
+//  )
+// })
+// function login(){
+//  const email = document.getElementById('email').value
+//  const password = document.getElementById('password').value
+//  firebase.auth().signInWithEmailAndPassword(email, password)
+// }
